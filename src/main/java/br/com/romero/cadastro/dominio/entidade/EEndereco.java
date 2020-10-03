02ed9d0ca196b6b3987a623af32b1d71ce5b9b63
gabrielmoraes <gabrielbar62@gmail.com>
2020-10-03 17:17:22 -0300

    class EP

---
+++ src/main/java/br/com/romero/cadastro/dominio/entidade/EEndereco.java
@@ -0,0 +1,18 @@
+package br.com.romero.cadastro.dominio.entidade;
+
+import lombok.Builder;
+import lombok.EqualsAndHashCode;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@EqualsAndHashCode
+@Builder
+
+public class EEndereco {
+    String rua;
+    int numero;
+    String bairro;
+    String cidade;
+}
